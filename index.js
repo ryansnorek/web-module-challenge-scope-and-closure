c2bfbb870f7b6b6bc8770b5c646bf34d963e1a8f
Ryan <snorekr@gmail.com>
2021-09-10 17:26:54 -0400

    Completed Task 1

--- index.js
+++ index.js
@@ -28,11 +28,16 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+      Counter 1 will store the memory from the incremented value every time it's invoked.
+      Counter 2 does not store the memory and will only ever log 0.
   
   2. Which of the two uses a closure? How can you tell?
+      They both use closure because they both access variables outside their scope.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+
 */
 
 // counter1 code
@@ -45,12 +50,14 @@ function counterMaker() {
 
 const counter1 = counterMaker();
 
+
 // counter2 code
 let count = 0;
 
 function counter2() {
   return count++;
 }
+
 
 
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
